bc070eea4cbfa76af136451a039af88dde9471f0
KenVeraChan <kenveracha@gmail.com>
2026-01-12 00:58:17 +0100

    Modelo pasando hojas de un libro terminado

---
+++ 0006_ProyectoHTML5/libroModelo.js
@@ -0,0 +1,41 @@
+$(document).ready(function() 
+    {
+        var paginaMovimiento=0; //Variable para controlar la página en la que estamos
+        let posicionX=0;
+        let posicionY=0;
+        for(var index=0;index<$('.pagina').length;index++){
+            $(".pagina").each(function(index) {    //Coloca las paginas en orden de colocación de etiqueta HTML
+                $(this).css("z-index", $('.pagina').length-index);
+            });
+        }
+        $('.pagina').click(function(event){
+            //Primero detecta la posición del raton dentro del elemento a fin de poder saber si es pagina siguiente o anterior
+            posicionX = event.pageX;
+            posicionY = event.pageY;
+            //alert("Posición X dentro del libro: " + posicionX + " Posición Y dentro del libro: " + posicionY);
+            if(posicionX >= (($("#libroAbierto").width()/2)+(parseInt($("#libroAbierto").css("margin-left")))) && posicionX<=(($("#libroAbierto").width())+(parseInt($("#libroAbierto").css("margin-left")))))
+            {  
+            // Si la posición X del clic es mayor que la mitad del libro (clic en la izquierda) y menor que el ancho total + el margin left del #libroAbierto
+            //Si se posiciona en otra zona en vertical, trabajar con la variable Y
+            //Pasa la pagina siguiente
+            $(this).toggleClass('rotado');
+            $(this).toggleClass('decolorado');
+            $(this).toggleClass('invisible');
+            paginaMovimiento++;   
+            $(this).css("z-index", $('.pagina').length+paginaMovimiento); //Ajusta el z-index para que las paginas ya pasadas queden debajo
+            //alert("Página siguiente número: " + paginaMovimiento);
+            }
+            else if(posicionX < (($("#libroAbierto").width()/2)+(parseInt($("#libroAbierto").css("margin-left")))) && posicionX>=0)
+            {  
+            // Si la posición X del clic es menor que la mitad del libro (clic en la izquierda) pero mayor que cero + el margin left del #libroAbierto
+            //Si se posiciona en otra zona en vertical, trabajar con la variable Y
+            //Pasa la pagina anterior
+            $(this).toggleClass('rotado');
+            $(this).toggleClass('decolorado');
+            $(this).toggleClass('invisible');
+            paginaMovimiento--;
+            $(this).css("z-index", $('.pagina').length-paginaMovimiento); //Ajusta el z-index para que las paginas ya pasadas queden debajo
+            //alert("Página anterior número: " + paginaMovimiento);
+            }
+        });
+    });
